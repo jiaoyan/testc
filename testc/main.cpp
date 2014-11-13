0b5d572a3bf624b8350632dbddb557d3d8712088
王蛟炎 <wang.jiaoyan@uqee.com>
2014-11-13 20:19:05 +0800

    Initial Commit

---
+++ testc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testc
+//
+//  Created by 椒盐 on 14-11-13.
+//  Copyright (c) 2014年 椒盐. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
